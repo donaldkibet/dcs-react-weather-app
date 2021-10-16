7d2276fb48ee8b9c7527313a5feec374abc6e7a3
Donald Kibet <chelashawdonald@yahoo.com>
2021-10-16 19:32:55 +0300

    (feat) AppJS style enhancement

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CityList from './components/TopCities/CityList';
+import Geolocation from './components/GeoLocation/GeoLocation'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Geolocation/>
+      <CityList/>
     </div>
   );
 }
