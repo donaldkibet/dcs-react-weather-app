02c3ef869e6fd9051672f10b5383fe83114ffde2
Donald Kibet <chelashawdonald@yahoo.com>
2021-10-16 19:32:18 +0300

    (feat) Add routing functionality to the app

--- src/index.js
+++ src/index.js
@@ -1,22 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "./store/Store";
+import { BrowserRouter, Route } from "react-router-dom";
+import CityWeatherInfo from "./components/CityWeatherInfo/CityWeatherInfo";
+import App from "./App";
+import NavBar from "./components/NavBar/NavBar";
+import Search from "./components/Search/Search";
+import AddTopCity from "./components/TopCities/AddTopCity";
+import { SWRConfig } from "swr";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <SWRConfig>
+        <Provider>
+          <Route path="/" render={(props) => <NavBar {...props} />} />
+          <Route exact path="/" render={(props) => <App {...props} />} />
+          <Route
+            exact
+            path="/search"
+            render={(props) => <Search {...props} />}
+          />
+          <Route
+            exact
+            path="/add-city"
+            render={(props) => <AddTopCity {...props} />}
+          />
+          <Route
+            exact
+            path="/weather/:cityName"
+            render={(props) => <CityWeatherInfo {...props} />}
+          />
+        </Provider>
+      </SWRConfig>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
-
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
