d56940f241270fafbbb9f4ea545a2827eceedf76
Donald Kibet <chelashawdonald@yahoo.com>
2021-10-18 11:24:05 +0300

    (enhancement) fix slow geo-location

--- src/components/CurrentLocationWeather/CurrentLocationWeather.js
+++ src/components/CurrentLocationWeather/CurrentLocationWeather.js
@@ -3,18 +3,26 @@ import { useWeatherWithGeoCoordinates } from "../../hooks/useWeather";
 import WeatherDetails from "../WeatherDetails/WeatherDetails";
 import { AiOutlineLoading } from "react-icons/ai";
 import { useGeoLocation } from "../../hooks/useGeoLocation";
-import styles from './CurrentLocatonWeather.module.css'
+import styles from "./CurrentLocatonWeather.module.css";
+
 
 const CurrentLocationWeather = () => {
-  const { permissionGrated, coordinates} = useGeoLocation()
-  const { data, error:apiError} = useWeatherWithGeoCoordinates(coordinates,permissionGrated);
+  const { permissionGrated, coordinates } = useGeoLocation();
+  const { data, error: apiError } = useWeatherWithGeoCoordinates(
+    coordinates,
+    permissionGrated
+  );
   
   return (
     <>
       {permissionGrated && (
         <div className={styles.currentConditionWeatherWrapper}>
-          {!data && !apiError && <AiOutlineLoading size={55} title="loading spinner" />}
-          {data && <WeatherDetails data={data}  />}
+          {!data && !apiError && (
+            <>
+            <AiOutlineLoading size={55} title="loading spinner" /> loading...
+            </>
+          )}
+          {data && <WeatherDetails data={data} />}
         </div>
       )}
     </>
